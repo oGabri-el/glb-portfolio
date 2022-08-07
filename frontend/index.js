e61163faf1239667c7b049fb688fa6fdcb7ac4be
oGabri-el <gabrieldlbrj@gmail.com>
2022-08-07 18:25:36 -0300

    show iconContainer complete

--- frontend/index.js
+++ frontend/index.js
@@ -1,5 +1,7 @@
-let portfolioNav = document.querySelector('#portfolio-nav');
-let portfolioNavStyle = portfolioNav.style;
+const portfolioNav = document.querySelector('#portfolio-nav');
+const portfolioNavStyle = portfolioNav.style;
+let textContainer = document.querySelector(".textContainer");
+const iconContainer = document.querySelector(".icon-container");
 
 function openNav(){
     if (portfolioNavStyle.display === "block") {
@@ -9,3 +11,44 @@ function openNav(){
     }
 
 }
+
+// function showInfo() {
+//  if (textContainer1.style.display === "none"){
+
+//    textContainer1.style.display = "block";
+//  } else{
+//   textContainer1.style.display = "none";}
+// }
+function showInfo1() {
+ if (textContainer.style.width === "10px"){
+   textContainer.style.width = "500px";
+   textContainer.style.opacity = 1;
+   textContainerText.style.opacity = 1;
+
+ } else{
+   textContainer.style.width = "10px";
+   textContainer.style.opacity = 0;
+   textContainerText.style.opacity = 0;
+}}
+function showInfo2() {
+ if (textContainer.style.width === "10px"){
+   textContainer.style.width = "500px";
+   textContainer.style.opacity = 1;
+   textContainerText.style.opacity = 1;
+
+ } else{
+   textContainer.style.width = "10px";
+   textContainer.style.opacity = 0;
+   textContainerText.style.opacity = 0;
+}}
+function showInfo3() {
+ if (textContainer.style.width === "10px"){
+   textContainer.style.width = "500px";
+   textContainer.style.opacity = 1;
+   textContainerText.style.opacity = 1;
+
+ } else{
+   textContainer.style.width = "10px";
+   textContainer.style.opacity = 0;
+   textContainerText.style.opacity = 0;
+}}
